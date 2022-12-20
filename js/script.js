d724b228cd549669753a3d6c7eeb3f130a56106f
roberto.boston <roberto.bozzo97@tiscali.it>
2022-12-20 17:16:46 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const {
+    createApp
+} = Vue;
+
+createApp({
+    data(){
+        return {
+            list : [
+                {
+                   text: "comprare il latte",
+                   done : false
+
+                },
+                {
+                    text: "comprare il pane",
+                    done : false
+ 
+                },
+                {
+                    text: "comprare la frutta",
+                    done : false
+ 
+                },
+                {
+                    text: "comprare la carne",
+                    done : false
+ 
+                },
+                {
+                    text: "comprare il formaggio",
+                    done : false
+ 
+                }
+            ]
+
+        }
+    },
+    methods: {
+
+        
+    },
+}).mount("#app")
